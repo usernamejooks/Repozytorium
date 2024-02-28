552e089f789256f85604af9a4d0cc477ffd42965
Piotr Struminski <piotrstruminskiprawdziwy@gmail.com>
2024-02-28 13:24:00 +0100

    install commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point;
+        point=new Point( x:2.4, y:5.5);
+        System.out.println(point);
+    }
+}
---
+++ untitled/src/Segment.java
@@ -0,0 +1,9 @@
+public class Segment {
+    private Point start,end;
+    public Point getStart(Point start){
+        this.start=start;
+    }
+    public Point getend(){
+        return end;
+    }
+}
